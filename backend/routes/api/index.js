30293edb978c776fcd78b95182f69b3b359f1a3b
Tong Thanh <tongthanhkt@gmail.com>
2023-12-13 10:44:23 +0700

    add: test api

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,56 +1,59 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRoutes = require('./auth');
-const userRoutes = require('./user');
-const addressRoutes = require('./address');
-const newsletterRoutes = require('./newsletter');
-const productRoutes = require('./product');
-const categoryRoutes = require('./category');
-const brandRoutes = require('./brand');
-const contactRoutes = require('./contact');
-const merchantRoutes = require('./merchant');
-const cartRoutes = require('./cart');
-const orderRoutes = require('./order');
-const reviewRoutes = require('./review');
-const wishlistRoutes = require('./wishlist');
+const authRoutes = require("./auth");
+const userRoutes = require("./user");
+const addressRoutes = require("./address");
+const newsletterRoutes = require("./newsletter");
+const productRoutes = require("./product");
+const categoryRoutes = require("./category");
+const brandRoutes = require("./brand");
+const contactRoutes = require("./contact");
+const merchantRoutes = require("./merchant");
+const cartRoutes = require("./cart");
+const orderRoutes = require("./order");
+const reviewRoutes = require("./review");
+const wishlistRoutes = require("./wishlist");
 
+router.get("/test", (req, res) => {
+  res.send("Test api call here !");
+});
 // auth routes
-router.use('/auth', authRoutes);
+router.use("/auth", authRoutes);
 
 // user routes
-router.use('/user', userRoutes);
+router.use("/user", userRoutes);
 
 // address routes
-router.use('/address', addressRoutes);
+router.use("/address", addressRoutes);
 
 // newsletter routes
-router.use('/newsletter', newsletterRoutes);
+router.use("/newsletter", newsletterRoutes);
 
 // product routes
-router.use('/product', productRoutes);
+router.use("/product", productRoutes);
 
 // category routes
-router.use('/category', categoryRoutes);
+router.use("/category", categoryRoutes);
 
 // brand routes
-router.use('/brand', brandRoutes);
+router.use("/brand", brandRoutes);
 
 // contact routes
-router.use('/contact', contactRoutes);
+router.use("/contact", contactRoutes);
 
 // merchant routes
-router.use('/merchant', merchantRoutes);
+router.use("/merchant", merchantRoutes);
 
 // cart routes
-router.use('/cart', cartRoutes);
+router.use("/cart", cartRoutes);
 
 // order routes
-router.use('/order', orderRoutes);
+router.use("/order", orderRoutes);
 
 // Review routes
-router.use('/review', reviewRoutes);
+router.use("/review", reviewRoutes);
 
 // Wishlist routes
-router.use('/wishlist', wishlistRoutes);
+router.use("/wishlist", wishlistRoutes);
 
 module.exports = router;
